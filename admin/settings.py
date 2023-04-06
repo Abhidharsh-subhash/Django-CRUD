f2beddd00e0edc66f3d901cf15c8ba606b74add3
abhidharshss <abhidharsh1999@gmail.com>
2023-04-06 13:30:26 +0530

    port hided

--- admin/settings.py
+++ admin/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '3306',
+        'PORT': config('DB_PORT'),
     }
 }
 
